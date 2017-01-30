2893be77f8a03c5b70c4fd2cb24a2730c305e48b
Brian Silverman <bri@readysetstem.com>
2017-01-30 17:43:00 -0500

    server - basic BLE scan

--- WatchServer/app/src/main/java/com/readysetstem/watchserver/MainActivity.java
+++ WatchServer/app/src/main/java/com/readysetstem/watchserver/MainActivity.java
@@ -1,13 +1,65 @@
 package com.readysetstem.watchserver;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Call back for BLE Scan
+     * This call back is called when a BLE device is found near by.
+     */
+    private BluetoothAdapter.LeScanCallback mLeScanCallback = new BluetoothAdapter.LeScanCallback() {
 
+        @Override
+        public void onLeScan(final BluetoothDevice device, final int rssi,
+                             byte[] scanRecord) {
+            runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Toast.makeText(getApplicationContext(),
+                            String.valueOf(rssi), Toast.LENGTH_SHORT)
+                            .show();
+
+                }
+            });
+        }
+    };
+
+    /**
+     * Method to detect whether the device is phone or tablet
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BluetoothAdapter mBluetoothAdapter;
+
+        // Use this check to determine whether BLE is supported on the device.
+        if (!this.getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(getApplicationContext(), "device_ble_not_supported",
+                    Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        // Initializes a Blue tooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager)
+                getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        if (mBluetoothAdapter == null) {
+            // Device does not support Blue tooth
+            Toast.makeText(getApplicationContext(),
+                    "device_bluetooth_not_supported", Toast.LENGTH_SHORT)
+                    .show();
+            finish();
+        }
+
+        mBluetoothAdapter.startLeScan(mLeScanCallback);
     }
 }
