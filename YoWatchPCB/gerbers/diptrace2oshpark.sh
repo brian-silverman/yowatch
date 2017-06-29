88d8403786cc1c3dfd8e8ca536ebef34bdbc86a9
Brian Silverman <bri@readysetstem.com>
2017-06-29 11:38:38 -0400

    pcb - schematic/layout first cut, sent to fab (oshpark)

---
+++ YoWatchPCB/gerbers/diptrace2oshpark.sh
@@ -0,0 +1,21 @@
+TMPDIR=`mktemp -d /tmp/$0.XXXXXXXXX`
+
+# General Layers
+cp BoardOutline.gbr $TMPDIR/yowatch.GKO
+cp BottomMask.gbr   $TMPDIR/yowatch.GBS
+cp BottomSilk.gbr   $TMPDIR/yowatch.GBO
+cp TopMask.gbr      $TMPDIR/yowatch.GTS
+cp TopSilk.gbr      $TMPDIR/yowatch.GTO
+
+# Signal layers
+cp Top.gbr          $TMPDIR/yowatch.GTL
+cp GND.gbr          $TMPDIR/yowatch.GBL
+cp Inner.gbr        $TMPDIR/yowatch.G2L
+cp VCC.gbr          $TMPDIR/yowatch.G3L
+
+# Drill
+cp Through.drl      $TMPDIR/yowatch.XLN
+
+zip -j yowatch.zip $TMPDIR/*
+
+rm -rf $TMPDIR
